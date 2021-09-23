9be84a02b4502cb52bf131bd0b4cf61290afb5d9
manikandang14study <manikandang14study@gmail.com>
2021-09-23 17:31:03 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome, Learn React.!
+          Welcome, Learn React..!
         </a>
         <p>Hi All</p>
       </header>

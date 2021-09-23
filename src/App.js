8121645c0e9234692b790dd5b8787ad7f1e522ac
manikandang14study <manikandang14study@gmail.com>
2021-09-23 13:52:56 +0530

    Check pipeline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome, Learn React.!
         </a>
         <p>Hi All</p>
       </header>
